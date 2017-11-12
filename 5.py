78b125b69d4c56d327a052bbb72e9b3e2d832d25
manishbatra4 <manishbatra004@gmail.com>
2017-11-13 00:40:25 +0530

    Initial Commit

--- 5.py
+++ 5.py
@@ -6,3 +6,11 @@ for name in names:
     print(name)
 
 print("manish" in names)
+
+number = [1, 1, 1, 1]
+print(number)
+
+number = [1, 1, 1, 1]
+number2 = [2, 2, 2, 2]
+print(number + number2)
+print((number + number2) * 3)
