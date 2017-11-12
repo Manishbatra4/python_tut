e386d0168c2fe3b8ffc4e63869bfed54da712eca
manishbatra4 <manishbatra004@gmail.com>
2017-11-13 01:57:35 +0530

    Initial Commit

---
+++ 7. Range.py
@@ -0,0 +1,11 @@
+number = list(range(10))
+print(number)
+
+number = list(range(10, 50))
+print(number)
+
+number = list(range(10, 80, 5))
+print(number)
+
+number = list(range(1, 1000, 10))
+print(number)
