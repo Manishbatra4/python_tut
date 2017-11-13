bee372c27c592e98115b4e4b53915fd6c057daf2
manishbatra4 <manishbatra004@gmail.com>
2017-11-13 13:26:07 +0530

    Initial Commit

--- Exception Handling  File Handling/01. Errors  exceptions.py
+++ Exception Handling  File Handling/01. Errors  exceptions.py
@@ -4,7 +4,7 @@ def mul(a, b):
     except ZeroDivisionError:
         print("error hai bc aur main nhi bta rha kya error hai!")
     finally:
-        print("Haan Kutta ye Wala Block Chalega Kuch Bhe Kar Le")
+        print("Haan Haramkhor ye Wala Block Chalega Kuch Bhe Kar Le")
 
 
 mul(20, 0)
