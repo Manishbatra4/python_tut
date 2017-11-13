310e0f0edcaab41a304d227bfac01a25a0206b0c
manishbatra4 <manishbatra004@gmail.com>
2017-11-13 12:32:38 +0530

    Initial Commit

---
+++ basic/6. list_function.py
@@ -0,0 +1,16 @@
+fruits = ["Apple", "Mango", "Banana", "Grapes"]
+print(fruits)
+
+fruits.append("Pizza")
+print(fruits)
+
+fruits.append("Sandwich")
+print(fruits)
+
+fruits.insert(1, "Pasta")
+print(fruits)
+
+fruits.insert(4, "Burger")
+print(fruits)
+
+print(fruits.index("Pizza"))
