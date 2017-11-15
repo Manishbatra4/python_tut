156774f269df4a1ba3980a1acbc77124128e487f
manishbatra4 <manishbatra004@gmail.com>
2017-11-16 00:54:30 +0530

    Initial Commit

---
+++ miscellaneous/06. String Functions.py
@@ -0,0 +1,11 @@
+print("Hello There".replace("There", "World"))
+
+newstring = "Fuck Off"
+print(newstring)
+print("after Replacing : " + newstring.replace("Off", "On"))
+
+newstring = "This is Dope Man"
+print(newstring.startswith("This"))
+print(newstring.endswith("This"))
+print(newstring.upper())
+print(newstring.lower())
