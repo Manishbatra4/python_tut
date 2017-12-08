7391e8f5e78c7a699bcfdf0bc46a853ef4aa2bbd
manish <manishbatra004@gmail.com>
2017-12-09 03:04:53 +0530

    Initial Commit

--- functional Programming/Functional_programming.py
+++ functional Programming/Functional_programming.py
@@ -1,1 +1,36 @@
-print("hello")
+# Passing Function through another Function
+def add_ten(x):
+    return x + 10
+
+
+def twice(func, args):
+    return func(func(args))
+
+
+print(twice(add_ten, 10))
+
+
+# Lambda Function
+
+def sqrt(x):
+    return x ** 2
+
+
+print(sqrt(10))
+
+result = (lambda x: x ** 2)(30)
+print(result)
+
+
+# mappig
+def add(x):
+    return x * 2
+
+
+newlist = [10, 20, 30, 40]
+result = list(map(add, newlist))
+print(result)
+result = list(map((lambda x: x ** 2), newlist))
+print(result)
+
+# filter
